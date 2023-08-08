1a266fb30ce767e84a4d4b3f8911991f34e3119e
luidimso <luidi_mso@outlook.com>
2023-08-08 20:37:12 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/luidimso/checklist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.luidimso.checklist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/to_do_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Checklist extends StatelessWidget {
+  const Checklist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
