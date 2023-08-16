c38f87c61390b80c0637763ea00f65f8b32093f8
luidimso <luidi_mso@outlook.com>
2023-08-16 04:45:41 -0300

    Creating get function on TaskRepository

--- lib/repositries/task_repository.dart
+++ lib/repositries/task_repository.dart
@@ -4,15 +4,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/task.dart';
 
-class TaskRepository {
-  TaskRepository() {
-    SharedPreferences.getInstance().then((value) => sharedPreferences = value);
-  }
+const key = "task_list";
 
+class TaskRepository {
   late SharedPreferences sharedPreferences;
 
   void save(List<Task> tasks) {
     final String jsonString = json.encode(tasks);
-    sharedPreferences.setString("task_list", jsonString);
+    sharedPreferences.setString(key, jsonString);
+  }
+
+  Future<List<Task>> get() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    final String jsonString = sharedPreferences.getString(key) ?? "[]";
+    final List jsonDecoded = json.decode(jsonString) as List;
+    return jsonDecoded.map((e) => Task.fromJson(e)).toList();
   }
 }
